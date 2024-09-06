869d62f5fd40cf291a38b2abfc5249a69ac04523
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2024-09-06 14:59:33 -0400

    set `/api` as base path

--- backend/main.go
+++ backend/main.go
@@ -10,8 +10,11 @@ func main() {
 	// Initialize a new Fiber app
 	app := fiber.New()
 
+	// set `/api` as base path
+	api := app.Group("/api")
+
 	// Define a route for the GET method on the root path '/'
-	app.Get("/", func(c fiber.Ctx) error {
+	api.Get("/", func(c fiber.Ctx) error {
 		// Send a string response to the client
 		return c.SendString("Hello, World 👋!")
 	})
