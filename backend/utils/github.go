8706846ef97d3f77572eb39a3c6001f040fe42a8
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2024-09-11 17:36:32 -0400

    move GetData function

--- backend/utils/github.go
+++ backend/utils/github.go
@@ -1,10 +1,5 @@
 package utils
 
-import (
-	"encoding/json"
-	"net/http"
-)
-
 type Contributors struct {
 	Contributors []Contributor
 }
@@ -14,15 +9,4 @@ type Contributor struct {
 	Username string `json:"login"`
 	Avatar   string `json:"avatar_url"`
 	Url      string `json:"url"`
-}
-
-func GetData(url string, data interface{}) error {
-	response, err := http.Get(url)
-
-	if err != nil {
-		return err
-	}
-
-	defer response.Body.Close()
-	return json.NewDecoder(response.Body).Decode(data)
 }
